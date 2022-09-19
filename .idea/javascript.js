68b1d8464ea5edf98954186a39dc1cd890d8162a
Felix <felixfk@uia.no>
2022-09-19 14:37:02 +0200

    test

---
+++ .idea/javascript.js
@@ -0,0 +1,14 @@
+//hvis totalPris er storre enn 800, frakt er 0 ether hvis totalPris er mindre eller #lik 800, frakt er 20% av prisen
+
+function totalPris(varePris, antall) {
+varePris * antall
+}
+
+fun leggTilFrakt(tPris)
+{
+    if tPris <= 800{
+    tPris + (tPris * 0.2)
+else{
+    tPris
+}}}
+
